9dcdb4194662936d93a91c53f9ded18c777af40e
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-08-24 21:18:32 +0900

    upd

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Pandera-Tool is a package for [pandera](https://github.com/unionai-oss/pandera).
 
+- DataFrame type definition
+- Validation
+- Type coerce
+
 ## Installation
 
 ```
@@ -10,4 +14,80 @@ pip install pandera-tool
 
 ## Example
 
-### 
+### DataFrame type definition
+
+```python
+from io import StringIO
+from typing import Tuple
+import pandas as pd
+from pandera import Field, SchemaModel
+from pandera.errors import SchemaError
+from pandera.typing import Series
+from pandera_tool import check_annotations, dtype, to_dataframe, to_schema
+
+@to_dataframe
+class DataFrameIn(SchemaModel):
+    Name: Series[str] = Field()
+
+    class Config:
+        strict = True
+
+@to_dataframe
+class DataFrameOut(to_schema(DataFrameIn)):
+    Age: Series[int] = Field()
+
+    class Config:
+        strict = True
+```
+
+### Validation
+
+```python
+@check_annotations
+def func_ok(df: DataFrameIn) -> DataFrameOut:
+    return df.assign(Age=2)
+
+@check_annotations
+def func_err(df: DataFrameIn) -> DataFrameOut:
+    return df.assign(Age="")
+
+@check_annotations
+def func_err_output_tuple(df: DataFrameIn) -> Tuple[DataFrameOut]:
+    return (df.assign(Age=""),)
+
+df_ok = pd.DataFrame({"Name": ["1"]})
+df_ng = pd.DataFrame({"Name": [1]})
+
+func_ok(df_ok)  # OK
+
+try:
+    func_ok(df_ng)  # Input error
+except SchemaError as e:
+    print(e)
+
+try:
+    func_err(df_ok)  # Output error
+except SchemaError as e:
+    print(e)
+
+try:
+    func_err_output_tuple(df_ok)  # Output error
+except SchemaError as e:
+    print(e)
+```
+
+### Type coerce
+
+```python
+df1 = pd.read_csv(StringIO("Name\n1"))
+try:
+    func_ok(df1)  # Input error
+except SchemaError as e:
+    print(e)
+
+df2 = pd.read_csv(StringIO("Name\n1"), dtype=dtype(DataFrameIn))
+func_ok(df2)  # OK
+
+df3 = df_ng.astype(dtype(DataFrameIn))
+func_ok(df3)  # OK
+```
--- tests/test_pandera_tool.py
+++ tests/test_pandera_tool.py
@@ -1,5 +1,81 @@
-from pandera_tool import __version__
+from io import StringIO
+from typing import Tuple
+
+import pandas as pd
+import pytest
+from pandera import Field, SchemaModel
+from pandera.errors import SchemaError
+from pandera.typing import Series
+from pandera_tool import check_annotations, dtype, to_dataframe, to_schema
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@to_dataframe
+class DataFrameIn(SchemaModel):
+    Name: Series[str] = Field()
+
+    class Config:
+        strict = True
+
+
+@to_dataframe
+class DataFrameOut(to_schema(DataFrameIn)):
+    Age: Series[int] = Field()
+
+    class Config:
+        strict = True
+
+
+@check_annotations
+def func_ok(df: DataFrameIn) -> DataFrameOut:
+    return df.assign(Age=2)
+
+
+@check_annotations
+def func_err(df: DataFrameIn) -> DataFrameOut:
+    return df.assign(Age="")
+
+
+@check_annotations
+def func_err_output_tuple(df: DataFrameIn) -> Tuple[DataFrameOut]:
+    return (df.assign(Age=""),)
+
+
+@pytest.fixture()
+def df_ok():
+    return pd.DataFrame({"Name": ["1"]})
+
+
+@pytest.fixture()
+def df_ng():
+    return pd.DataFrame({"Name": [1]})
+
+
+def test_ok(df_ok):
+    func_ok(df_ok)  # OK
+
+
+def test_ng_input_err(df_ng):
+    with pytest.raises(SchemaError):
+        func_ok(df_ng)  # Input error
+
+
+def test_ng_output_err(df_ok):
+    with pytest.raises(SchemaError):
+        func_err(df_ok)  # Output error
+
+
+def test_ng_output_tuple(df_ok):
+    with pytest.raises(SchemaError):
+        func_err_output_tuple(df_ok)  # Output error
+
+
+def test_read_csv():
+    df1 = pd.read_csv(StringIO("Name\n1"))
+    with pytest.raises(SchemaError):
+        func_ok(df1)  # Input error
+    df2 = pd.read_csv(StringIO("Name\n1"), dtype=dtype(DataFrameIn))
+    func_ok(df2)  # OK
+
+
+def test_coerce(df_ng):
+    func_ok(df_ng.astype(dtype(DataFrameIn)))  # OK
